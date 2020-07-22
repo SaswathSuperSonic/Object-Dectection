d29cf06b1779b5c325e37f7ab42ca7bb726e3402
SaswathSuperSonic <67815102+SaswathSuperSonic@users.noreply.github.com>
2020-07-22 20:42:41 +0530

    Add files via upload

--- Object.js
+++ Object.js
@@ -7,15 +7,19 @@ function preload(){
 }
 
 function setup() {
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
     objectDetector = ml5.objectDetector('cocosd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results) {
@@ -27,19 +31,24 @@ function gotResult(error, results) {
 }
 
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
     if(status !="") 
     {
+        objectDetector.detect(video, gotResult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
         for (i = 0; i < objects.lenght; i++);
         {
             document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are : "+ objects.length;
 
-            fill("#FF0000");
+            fill(r, g, b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            stroke("#FF0000");
+            stroke(r, g, b);
             rect(objects[i].x, object[i].y, objects[i].width, objects[i].height);
         }
     }
